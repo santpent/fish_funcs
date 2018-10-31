7c6c787650ecc124963ee222458d4480df186dd2
santpent <44591078+santpent@users.noreply.github.com>
2018-10-31 15:23:09 +0000

    Update fish_funcs.sh

--- fish_funcs.sh
+++ fish_funcs.sh
@@ -3,8 +3,7 @@ function string_split
 	set -l options 'n/nth=' 'm/max=' 'r/right' 'q/quiet' 'w/whitespace' 'h/help'
 	argparse -n string_split $options -- $argv
 	or return
-   
-   
+      
 	if set -q _flag_help
 		echo "usage: \$ <commands> | string_split <column_number>"
 		echo "Example:"
