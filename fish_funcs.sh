415c6bda88a445918a6d52bfb0ae7572fcc705cf
santpent <44591078+santpent@users.noreply.github.com>
2018-10-31 15:21:45 +0000

    Update fish_funcs.sh

--- fish_funcs.sh
+++ fish_funcs.sh
@@ -1,22 +1,22 @@
-
 function string_split
 
-	set -l options 'n/nth=' 'm/max=' 'r/right' 'q/quiet' 'w/whitespace'
+	set -l options 'n/nth=' 'm/max=' 'r/right' 'q/quiet' 'w/whitespace' 'h/help'
 	argparse -n string_split $options -- $argv
 	or return
    
-	# usage: <commands> | string_split <column_number>
-	# ls -l | get_column 5
-	# can use fish array index slicing, eg 2..4, 3..-1. Can't omit indexes.
+	if set -q _flag_help
+		echo "usage: \$ <commands> | string_split <column_number>"
+		echo "Example:"
+		echo "\$ echo 'a, b, c, d' | string_split ', ' 3 # returns c"
+		return
+	end
 	
 	set -l separator $argv
-	
 	set -l max_switch
 	
 	if set -q _flag_max
 		set max_switch --max
 	end
-	
 	
 	cat /dev/stdin | while read -l line
 
@@ -28,18 +28,13 @@ function string_split
 
 		if set -q _flag_nth
 			
-			# need option here to ignore lines in which the separator is not found
-			
 			if echo $line | string match -q -e $separator
 			
 				set -l split_array ( echo $line | string split $max_switch $_flag_max $_flag_right $_flag_quiet $separator --)
 				echo $split_array[$_flag_nth]
-			
 			end
-			
 		else
 			echo $line | string split $max_switch $_flag_max $_flag_right $_flag_quiet $separator --
 		end
-		
 	end
 end
