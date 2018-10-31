282b1987d3817d85f438b76b9cc1e5e88c689970
santpent <44591078+santpent@users.noreply.github.com>
2018-10-31 21:19:56 +0000

    Update fish_funcs.sh

--- fish_funcs.sh
+++ fish_funcs.sh
@@ -1,13 +1,14 @@
+
 function string_split
 
 	set -l options 'n/nth=' 'm/max=' 'r/right' 'q/quiet' 'w/whitespace' 'h/help'
 	argparse -n string_split $options -- $argv
 	or return
-      
+   
 	if set -q _flag_help
-		echo "usage: \$ <commands> | string_split <column_number>"
-		echo "Example:"
-		echo "\$ echo 'a, b, c, d' | string_split ', ' 3 # returns c"
+		echo "Usage: same as fish string split, with added switches"
+		echo "-n / --nth: returns specific column(s) from split."
+		echo "-w / --whitespace: reduce combined whitespace into a single space, useful for processing tabulated output."
 		return
 	end
 	
@@ -36,5 +37,84 @@ function string_split
 		else
 			echo $line | string split $max_switch $_flag_max $_flag_right $_flag_quiet $separator --
 		end
+	end
+end
+
+
+function string_find
+	
+	set -l options 'k/keywords'
+	argparse -n string_find $options -- $argv
+	
+	set -l find_from $argv[1]
+	
+	if set -q $argv[2]
+		set -l find_to $argv[2]
+	end
+	
+	cat /dev/stdin | while read -l line
+	
+		if echo $line | string match --quiet --entire "$find_from" --
+			
+			set found (echo $line | string split $find_from --)[2]
+
+			if test -n "$find_to"
+				set found (echo $found | string split $find_to --)[1]
+			end
+			
+			if set -q _flag_keywords
+				echo $find_from$found$find_to
+			else
+				echo $found
+			end
+		end
+	end
+end
+
+
+function funced2 --wraps functions
+
+	set -l options 'e/editor='
+	argparse -n funced2 $options -- $argv
+	
+	set -l editor
+	if set -q _flag_editor
+		set editor $_flag_editor
+	else
+		set editor fish
+	end
+	
+	if not which $editor >/dev/null # account for $editor not found
+		funced --editor $editor $argv
+		return
+	end
+
+	if not functions $argv >/dev/null # account for function not found
+		funced --editor $editor $argv
+		return
+	end
+	
+	set -l defined ( functions $argv | grep '^# Defined in' | string split '# Defined in ' -- )[2]
+	
+	set -l defined ( echo $defined | string split ' @ line ')
+
+	set -l filename $defined[1]
+	set -l line_no $defined[2]
+	
+	switch $editor
+		case vi vim emacs
+			eval $editor +$line_no $filename &
+		case kate geany notepadqq
+			eval $editor --line $line_no $filename &
+			# notepadqq --line $line_no $filename &
+			# notepadqq $filename --line $line_no  &
+		case scite
+			eval $editor -open $filename -goto:$line_no & # note: this wipes the current scite session
+		case atom subl
+			eval $editor $filename:$line_no & # sublime text
+		case gedit
+			eval $editor $filename &
+		case '*'
+			funced --editor $editor $filename
 	end
 end
